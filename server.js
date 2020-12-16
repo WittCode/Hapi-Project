2a67ff0825b1057cad0baa7a64f2feb9926303c9
Tom Wittmann <WittCode@gmail.com>
2020-12-15 21:24:08 -0500

    Finalized server file for part 2 of the series.

--- server.js
+++ server.js
@@ -2,20 +2,62 @@
 
 const Hapi = require('@hapi/hapi');
 
-const init = async() => {
+const init = async () => {
 
     const server = Hapi.server({
         port: 1234,
         host: "localhost"
     });
 
-    server.route({
-        method: 'GET',
-        path: '/',
-        handler: (request, h) => {
-            return "<h1>Hello World!</h1>";
+
+    server.route([
+        {
+            method: 'GET',
+            path: '/',
+            handler: (request, h) => {
+                return "<h1>Welcome to my website!</h1>";
+            }
+        },
+        {
+            method: 'GET',
+            path: '/users/{user?}',
+            handler: (request, h) => {
+                if (request.params.user) {
+                    return `<h1>Hello ${request.params.user}</h1>`;
+                } else {
+                    return "<h1>Hello Stranger!</h1>";
+                }
+
+            }
+        },
+        {
+            method: 'GET',
+            path: '/queryParams',
+            handler: (request, h) => {
+                if (request.query.firstName && request.query.lastName) {
+                    let { firstName, lastName } = request.query;
+                    return `<h1>Hello ${firstName} ${lastName}!`;
+                } else {
+                    return '<h1>Who are you?</h1>'
+                }
+            }
+        },
+        {
+            method: 'GET',
+            path: '/redirectMe',
+            handler: (request, h) => {
+                return h.redirect('/');
+            }
+        },
+        {
+            method: '*',
+            path: '/{any*}',
+            handler: (request, h) => {
+                return "<h1>You must be lost!</h1>";
+            }
         }
-    });
+
+    ]);
 
     await server.start();
     console.log("Listening on " + server.info.uri);
